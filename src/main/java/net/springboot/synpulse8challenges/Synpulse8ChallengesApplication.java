6c84d8460669c681c9555164c6069b4d1b923903
Jack Lee <wjlee0405@gmail.com>
2023-02-09 21:21:39 +0800

    Setup Repository

---
+++ src/main/java/net/springboot/synpulse8challenges/Synpulse8ChallengesApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.synpulse8challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Synpulse8ChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Synpulse8ChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/springboot/synpulse8challenges/Synpulse8ChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.synpulse8challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Synpulse8ChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
